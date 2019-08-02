a9443940dbfaad953368468981e656ca79c42a4c
berkelmas <berkelmas96@gmail.com>
2019-08-02 22:08:46 +0300

    fixed wsgi.py file for gunicorn start

--- bosanmaAvukatimBackend/wsgi.py
+++ bosanmaAvukatimBackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cezaAvukatimBackend project.
+WSGI config for bosanmaAvukatimBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cezaAvukatimBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bosanmaAvukatimBackend.settings')
 
 application = get_wsgi_application()
